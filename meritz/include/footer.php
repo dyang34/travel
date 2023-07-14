50f488f13c600ad13571d9ba5a8af4d86cddc196
dyang34 <dyang34@naver.com>
2023-07-14 10:14:05 +0900

    footer 수정.

--- meritz/include/footer.php
+++ meritz/include/footer.php
@@ -1,25 +1,29 @@
 
     <!-- /* footer start -->
     <footer>
-      <ul class="footer-menu">
-        <li><a href="/travel/meritz/terms/agreement.php">이용약관</a></li>
-        <li><a href="/travel/meritz/terms/privacy.php">개인정보취급방침</a></li>
-        <li><a href="/travel/meritz/files/overseas.pdf" target="_blank">해외여행보험약관</a></li>
-        <li><a href="/travel/meritz/files/domestic.pdf" target="_blank">국내여행보험약관</a></li>
-      </ul>
-      <ul class="footer-info">
-        <li>
-          <span>주소. 서울특별시 중구 광희동2가 266 성우빌딩 10F</span>
-          <span>대표. 김정훈</span>
-          <span>사업자등록번호. 118-88-00158</span>
-        </li>
-        <li>
-          <span>이메일. <a href="mailto:help@udirect.co.kr">help@udirect.co.kr</a></span>
-          <span>전화. 1800-9010</span>
-          <span>팩스. 02-2088-1673</span>
-        </li>
-        <li>COPYRIGHT ⓒ UDIRECT Co., Ltd. ALL RIGHTS RESERVED</li>
-      </ul>
+        <ul class="footer-menu">
+            <li><a href="/travel/meritz/terms/agreement.php">이용약관</a></li>
+            <li><a href="/travel/meritz/terms/privacy.php">개인정보취급방침</a></li>
+            <li><a href="/travel/meritz/files/overseas.pdf" target="_blank">해외여행보험약관</a></li>
+            <li><a href="/travel/meritz/files/domestic.pdf" target="_blank">국내여행보험약관</a></li>
+        </ul>
+        <ul class="footer-info">
+            <li>
+                <span>보험대리점 : ㈜비아이에스</span>
+                <span>주소 : 서울특별시 중구 광희동2가 266 성우빌딩 10F</span>
+                <span>대표 : 김성일</span>
+            </li>
+            <li>
+                <span>사업자등록번호 : 636-87-00912</span>
+                <span>통신판매신고 번호 : 2023-서울중구-1112</span>
+            </li>
+            <li>
+                <span>이메일 : <a href="mailto:help@udirect.co.kr">help@udirect.co.kr </a></span>
+                <span>전화 : <a href="tel:1800-9010">1800-9010</a></span>
+                <span>팩스 : 02-2088-1673</span>
+            </li>
+            <li>COPYRIGHT ⓒ UDIRECT Co., Ltd. ALL RIGHTS RESERVED.</li>
+        </ul>
     </footer>
     <!-- footer end **/ -->
 
--- meritz/m/include/footer.php
+++ meritz/m/include/footer.php
@@ -8,18 +8,20 @@
 				<div class="address">
 					<ul>
 						<li>
+							<span>보험대리점 : ㈜비아이에스</span>
 							<span>주소 : 서울특별시 중구 광희동2가 266 성우빌딩 10F</span>
-							<span>대표 : 김정훈</span>
+							<span>대표 : 김성일</span>
 						</li>
 						<li>
-							<span>사업자등록번호 : 118-88-00158</span>
-							<span>이메일 : <a href="mailto:help@udirect.co.kr">help@udirect.co.kr</a></span>
+							<span>사업자등록번호 : 636-87-00912</span>
+							<span>통신판매신고 번호 : 2023-서울중구-1112</span>
 						</li>
 						<li>
-							<span>전화 : 1800-9010</span>
+							<span>이메일 : <a href="mailto:help@udirect.co.kr">help@udirect.co.kr </a></span>
+							<span>전화 : <a href="tel:1800-9010">1800-9010</a></span>
 							<span>팩스 : 02-2088-1673</span>
 						</li>
-						<li class="copyright">COPYRIGHT ⓒ UDIRECT Co., Ltd. ALL RIGHTS RESERVED</li>
+						<li class="copyright">COPYRIGHT ⓒ UDIRECT Co., Ltd. ALL RIGHTS RESERVED.</li>
 					</ul>
 				</div>
 			</footer>
